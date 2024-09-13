2805ccf540e0d77ff07f9392bf1ac43c9a378a52
Asif Hasan <118726353+asifhasan002@users.noreply.github.com>
2024-09-13 12:11:47 +0600

    Add files via upload

---
+++ anim-text/anim-testUITests/anim_testUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  anim_testUITestsLaunchTests.swift
+//  anim-testUITests
+//
+//  Created by rootnext05 on 11/9/24.
+//
+
+import XCTest
+
+final class anim_testUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
