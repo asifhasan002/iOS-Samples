0eb3106dbf0fb341183e683e332c62378c937977
asifhasan002 <118726353+asifhasan002@users.noreply.github.com>
2024-09-15 14:50:57 +0600

    done

--- anim-text/anim-test/WordColorPair.swift
+++ anim-text/anim-test/WordColorPair.swift
@@ -23,8 +23,10 @@ enum WordColorPair: Int, CaseIterable {
             return ("MINT", UIColor(red: 0.8, green: 0.9, blue: 1.0, alpha: 1.0))
         case .word7:
             return ("BOLD", UIColor.blue.brighten(by: 0.3))
-        case .word8, .word9:
+        case .word8:
             return ("GLOW", UIColor.magenta)
+        case .word9:
+            return ("WAVE", UIColor.magenta)
         case .word10, .word11:
             return ("FIZZ", UIColor.brown)
         }
